1b8f520eaa734c179af876eee84b211438210b6c
nickvonklemp <nick.vonklemp@gmail.com>
2016-06-06 07:59:52 +0200

    Commit 022 Started PSO tuning, created sub folders for individual controllers Introduced position controller PD_Diagonal Attitude tuned PD_Diagonal Position tuned Control Allocation Block Complete

--- Controllers/Position Controllers/PD Position/PSO_PD_Position_workspace.m
+++ Controllers/Position Controllers/PD Position/PSO_PD_Position_workspace.m
@@ -1,46 +1,51 @@
-% k0=[1 1 1];
-% a0=[1 1 1];
-%persistent x vn K1 K2 K3 a1 a2 a3 pbest gbest bestG bestP
+%persistent x vn P1 P2 P3 D1 D2 D3 pbest gbest bestG bestP
 %First run through
-% x = [k0(1) k0(2) k0(3) a0(1) a0(2) a0(3)];
-x =  [2.7649    2.8837    1.7051    3.1709    4.2307    3.3348];
-vn = [0.1 0.1 0.1 0.1 0.1 0.1].';
-K1=[0 x(1) 0 0];
-K2=[0 0 x(2) 0];
-K3=[0 0 0 x(3)];
-a1=[0 x(4) 0 0];
-a2=[0 0 x(5) 0];
-a3=[0 0 0 x(6)];
-Rb0 = 0;
+%x = [0.6732    0.6641    0.1283    2.0737    2.0761    0.3630];
+x =  [1.2117    0.9507    1.3854    2.7244    2.6296    3.1198];
+vn = [0.1 0.1 0.1 0.1 0.1 0.1].'; %Velocity
+P1=[0 x(1) 0 0];
+P2=[0 0 x(2) 0];
+P3=[0 0 0 x(3)];
+D1=[0 x(4) 0 0];
+D2=[0 0 x(5) 0];
+D3=[0 0 0 x(6)];
+Eb0 = [0 0 0 0]; %Starting Position. Aims for Eb = [3 3 3]
 pGe=0;
 pQe=0;
 pWe=0;
 i=1;
-for psi = 20:60:360
-    for theta = 20:60:360
-        for rho = 20:60:360
-            Rb0 = [0 psi theta rho]; %creates starting euler angles (rad)
+for psi = 20:130:360
+    rz = [cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1];
+    for theta = 20:130:360
+        ry = [cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta)];
+        for rho = 20:130:360
+            rx = [1 0 0; 0 cos(rho) sin(rho); 0 -sin(rho) cos(rho)];
+            R0 = rz*ry*rx;
+            dR = R0*([0 0 5].');
+            Eb0 = [3 3 3] + dR.';
             %hws = Simulink.ModelWorkspace;
             hws = get_param(bdroot, 'modelworkspace');
             hws.DataSource = 'MAT-File';
             hws.FileName = 'co-effs';
-            hws.assignin('Rb0', Rb0);
-            hws.assignin('K1', K1);
-            hws.assignin('K2', K2);
-            hws.assignin('K3', K3);
-            hws.assignin('a1', a1);
-            hws.assignin('a2', a2);
-            hws.assignin('a3', a3);
+            hws.assignin('Eb0', [0 Eb0]);
+            hws.assignin('P1', P1);
+            hws.assignin('P2', P2);
+            hws.assignin('P3', P3);
+            hws.assignin('D1', D1);
+            hws.assignin('D2', D2);
+            hws.assignin('D3', D3);
             hws.saveToSource;
             hws.reload;
             try
-                [Out]=sim('PD_Diagonal','ReturnWorkspaceOutputs', 'on','SrcWorkspace','base'); 
+                [Out]=sim('PD_Position','ReturnWorkspaceOutputs', 'on','SrcWorkspace','base'); 
                 i = i + 1;
                 disp(i);
+                disp(Out.get('Ge'));
                 pGe=pGe+Out.get('Ge');
                 pQe=pWe+Out.get('Qe');
                 pWe=pQe+Out.get('We');
             catch ME
+                disp('not ok');
                 if (strcmp(ME.identifier,'Simulink:Engine:DerivNotFinite'))
                     disp('Null');
                     break;
@@ -71,35 +76,41 @@ disp(bestG);
 while(true)
     vn = vn + 1*(rand(6,1)).*(pbest-x).' + 2*(rand(6,1)).*(gbest-x).';
     x = x + 0.1*vn.';
-    K1=[0 x(1) 0 0];
-    K2=[0 0 x(2) 0];
-    K3=[0 0 0 x(3)];
-    a1=[0 x(4) 0 0];
-    a2=[0 0 x(5) 0];
-    a3=[0 0 0 x(6)];
+    P1=[0 x(1) 0 0];
+    P2=[0 0 x(2) 0];
+    P3=[0 0 0 x(3)];
+    D1=[0 x(4) 0 0];
+    D2=[0 0 x(5) 0];
+    D3=[0 0 0 x(6)];
     pGe=0;
     pQe=0;
     pWe=0;
     i=1;
-    for psi = 20:60:360
-        for theta = 20:60:360
-            for rho = 20:60:360
+    for psi = 20:130:360
+        for theta = 20:130:360
+            for rho = 20:130:360
                 Rb0 = [0 psi theta rho]; %creates starting euler angles (rad)
+                rz = [cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1];
+                ry = [cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta)];
+                rx = [1 0 0; 0 cos(rho) sin(rho); 0 -sin(rho) cos(rho)];
+                R0 = rz*ry*rx;
+                dR = R0*([0 0 5].');
+                Eb0 = [3 3 3] + dR.';
                 %hws = Simulink.ModelWorkspace;
                 hws = get_param(bdroot, 'modelworkspace');
                 hws.DataSource = 'MAT-File';
                 hws.FileName = 'co-effs';
-                hws.assignin('Rb0', Rb0);
-                hws.assignin('K1', K1);
-                hws.assignin('K2', K2);
-                hws.assignin('K3', K3);
-                hws.assignin('a1', a1);
-                hws.assignin('a2', a2);
-                hws.assignin('a3', a3);
+                hws.assignin('Eb0', [0 Eb0]);
+                hws.assignin('P1', P1);
+                hws.assignin('P2', P2);
+                hws.assignin('P3', P3);
+                hws.assignin('D1', D1);
+                hws.assignin('D2', D2);
+                hws.assignin('D3', D3);
                 hws.saveToSource;
                 hws.reload;
                 try
-                    [Out]=sim('PD_Diagonal','ReturnWorkspaceOutputs', 'on','SrcWorkspace','base'); 
+                    [Out]=sim('PD_Position','ReturnWorkspaceOutputs', 'on','SrcWorkspace','base'); 
                     i = i + 1;
                     pGe=pGe+Out.get('Ge');
                     pQe=pWe+Out.get('Qe');
